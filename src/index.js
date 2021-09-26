854338c644499245c1bee710e7f29e276cacc488
AxiomeDuChoix <cesc01002@gmail.com>
2021-09-26 22:19:35 +0200

    1st change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TodoApp from './Tuco'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <TodoApp />,
   document.getElementById('root')
 );
 
