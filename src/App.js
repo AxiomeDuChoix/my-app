9584b62abfc237a9739a96a479b28b87f24a4dd3
AxiomeDuChoix <cesc01002@gmail.com>
2021-09-26 17:37:06 +0200

    1st change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload TUCO.
         </p>
         <a
           className="App-link"
@@ -24,79 +24,3 @@ function App() {
 
 export default App;
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  render() {
-    return React.createElement(
-      "div",
-      null,
-      React.createElement(
-        "h3",
-        null,
-        "TODO"
-      ),
-      React.createElement(TodoList, { items: this.state.items }),
-      React.createElement(
-        "form",
-        { onSubmit: this.handleSubmit },
-        React.createElement(
-          "label",
-          { htmlFor: "new-todo" },
-          "What needs to be done?"
-        ),
-        React.createElement("input", {
-          id: "new-todo",
-          onChange: this.handleChange,
-          value: this.state.text
-        }),
-        React.createElement(
-          "button",
-          null,
-          "Add #",
-          this.state.items.length + 1
-        )
-      )
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.text.length === 0) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
-  }
-}
-
-class TodoList extends React.Component {
-  render() {
-    return React.createElement(
-      "ul",
-      null,
-      this.props.items.map(item => React.createElement(
-        "li",
-        { key: item.id },
-        item.text
-      ))
-    );
-  }
-}
-
-ReactDOM.render(React.createElement(TodoApp, null), document.getElementById('todos-example'));
